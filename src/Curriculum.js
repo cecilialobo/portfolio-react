a28a1e44afc898c40c186eb6d6bff2726f9b1b77
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-09 22:46:27 +0100

    Add Curriculum Component

---
+++ src/Curriculum.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import './About.css';
+import './Curriculum.css';
+
+function Curriculum (props){
+    return(
+        <div className="curriculum">
+            <h3 className="subtitle">{props.subtitleCv}</h3>
+            <p className="textp">{props.textCv1}</p>
+            <p className="textp">{props.textCv2}</p>
+        </div>
+    );
+}
+
+export default Curriculum;
