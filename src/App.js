34669ab174224cf08650ff0320714a6d1aeb187b
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-11 00:08:36 +0100

    Add image to about page and style with grid

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Education from './components/Education/Education';
 import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
+import Photo from './assets/template.png';
 
 import './App.css';
 import './components/About/About.css';
@@ -17,7 +18,10 @@ function App() {
         <i className="fas fa-angle-down" id="arrowBtn"></i>
       </div>
 
+      <div className="homeGrid">
       <About></About>
+      <img className="img" src={Photo} alt="Cecilia's image"></img>
+      </div>
 
       <div className="curriculum">
         <Header id1="landing" id2="contact2" title1="Home" title2="Contact"></Header>
