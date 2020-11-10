911bf15f0171b8682c051f5ef099fe805ee2f39e
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-10 15:36:02 +0100

    About.js to static component

--- src/App.js
+++ src/App.js
@@ -7,22 +7,12 @@ import './components/About/About.css';
 function App() {
   return (
     <div className="App">
-      <div className="section1">
+      <div className="landing">
         <p className="frontend-title">Frontend Developer</p>
         <h1 className="title">Cecilia Lobo</h1>
       </div>
 
-      <div className="section2">
-        <h2 className="heading">Hello world!</h2>
-        <About subtitle="About Me" textp="I'm a brazilian Frontend Developer passionate about technology, based in Stockholm/Sweden."></About>
-        <br/>
-        <About subtitle="Changing Careers" textp="Technology is a new thing for me. I used to work as a Dietitian and I have a M.Sc. in Nutrition and Health. 
-        While doing my master's, I discovered the world of coding and decided that this was the right path for me. 
-        Currently, I'm a full-time student at Hyper Island. Most of my work is using HTML, CSS and javaScript."></About>
-        <br/>
-        <About subtitle="Travel" textp="Another big passion of mine is to travel and be able to discover different cultures. 
-        I have visited around 30 countries and hope to continue visiting more and more."></About>
-      </div>
+        <About></About>
       
       <div className="section3">
         <h2 className="heading">Education</h2>
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './About.css';
 
-function About(props) {
+function About() {
     return (
-    <div className="About">
-        <h3 className="subtitle">{props.subtitle}</h3>
-        <p className="textp">{props.textp}</p>
-    </div>
+        <div className="About">
+
+            <h2 className="heading">Hello world!</h2>
+            <h3 className="subtitle">About me</h3>
+            <p className="textp">I'm a brazilian Frontend Developer passionate about technology, based in Stockholm/Sweden.</p>
+            <br/>
+            <h3 className="subtitle">Changing Careers</h3>
+            <p className="textp">Technology is a new thing for me. I used to work as a Dietitian and I have a M.Sc. in Nutrition and Health. 
+                While doing my master's, I discovered the world of coding and decided that this was the right path for me. 
+                Currently, I'm a full-time student at Hyper Island. Most of my work is using HTML, CSS and javaScript.</p>
+            <br/>
+            <h3 className="subtitle">Travel</h3>
+            <p className="textp">Another big passion of mine is to travel and be able to discover different cultures. 
+                I have visited around 30 countries and hope to continue visiting more and more.</p>
+
+        </div>
     );
 }
 
