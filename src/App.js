aa55e3e916a3279cf879b0962b1b300e8b5c4607
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-10 23:27:50 +0100

    Add Header component to landing page, contact and curriculum

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './components/About/About';
 import Education from './components/Education/Education';
 import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
 
 import './App.css';
 import './components/About/About.css';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
       <div className="landing">
+        <Header id1="curriculum" id2="contact" title1="Curriculum" title2="Contact"></Header>
         <p className="frontend-title">Frontend Developer</p>
         <h1 className="title">Cecilia Lobo</h1>
         <i className="fas fa-angle-down" id="arrowBtn"></i>
@@ -18,11 +20,15 @@ function App() {
       <About></About>
 
       <div className="curriculum">
+        <Header id1="landing" id2="contact2" title1="Home" title2="Contact"></Header>
         <Education></Education>
         <Experience></Experience>
       </div>
 
-      <Contact></Contact>
+      <div className="contactDiv">
+        <Header id1="landing2" id2="curriculum2" title1="Home" title2="Curriculum"></Header>
+        <Contact></Contact>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './index.css';
 import './Header.css';
 
 function Header(props) {
     return(
         <div className="Header">
-            <a href={props.href1}>{props.title1}</a>
-            <a href={props.href2}>{props.title2}</a>
+            <a href="" id={props.id1}>{props.title1}</a>
+            <a href="" id={props.id2}>{props.title2}</a>
         </div>
     );
 }
