2ac92250c7f3147cd7cf7b3c7d44fb2c6f67052c
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-12 23:21:38 +0100

    Add logic to header buttons

--- src/App.js
+++ src/App.js
@@ -8,30 +8,61 @@ import Photo from './assets/template.png';
 import './App.css';
 import './resposive.css';
 import './components/About/About.css';
+import { useRef } from 'react';
 
 function App() {
+
+  const scrollIntoView = (divRef) => {
+    divRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  const homeRef = useRef();
+  const aboutRef = useRef();
+  const curriculumRef = useRef();
+  const contactRef = useRef();
+
   return (
     <div className="App">
-      <div className="landing">
-        <Header id1="curriculum" id2="contact" title1="Curriculum" title2="Contact" />
+      <div className="landing" ref={homeRef}>
+        <Header id1="curriculum"
+          id2="contact"
+          title1="Curriculum"
+          title2="Contact"
+          handleClickMenu1={() => scrollIntoView(curriculumRef)}
+          handleClickMenu2={() => scrollIntoView(contactRef)}
+        />
+
         <p className="frontend-title">Frontend Developer</p>
         <h1 className="title">Cecilia Lobo</h1>
-        <i className="fas fa-angle-down" id="arrowBtn"></i>
+        <i className="fas fa-angle-down" id="arrowBtn" onClick={() => { scrollIntoView(aboutRef) }}></i>
       </div>
 
-      <div className="homeGrid">
-      <About />
-      <img className="img" src={Photo} alt="Cecilia smiling"></img>
+      <div className="homeGrid" ref={aboutRef}>
+        <About />
+        <img className="img" src={Photo} alt="Cecilia smiling"></img>
       </div>
 
-      <div className="curriculum">
-        <Header id1="landing" id2="contact2" title1="Home" title2="Contact" />
+      <div className="curriculum" ref={curriculumRef}>
+        <Header id1="landing"
+          id2="contact2"
+          title1="Home"
+          title2="Contact"
+          handleClickMenu1={() => scrollIntoView(homeRef)}
+          handleClickMenu2={() => scrollIntoView(contactRef)}
+        />
         <Education />
         <Experience />
       </div>
 
-      <div className="contactDiv">
-        <Header id1="landing2" id2="curriculum2" title1="Home" title2="Curriculum" />
+      <div className="contactDiv" ref={contactRef}>
+        <Header
+          id1="landing2"
+          id2="curriculum2"
+          title1="Home"
+          title2="Curriculum"
+          handleClickMenu1={() => scrollIntoView(homeRef)}
+          handleClickMenu2={() => scrollIntoView(curriculumRef)}
+        />
         <Contact />
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 function Header(props) {
     return(
         <div className="Header">
-            <button href="" id={props.id1}>{props.title1}</button>
-            <button href="" id={props.id2}>{props.title2}</button>
+            <button id={props.id1} onClick={props.handleClickMenu1}>{props.title1}</button>
+            <button id={props.id2} onClick={props.handleClickMenu2}>{props.title2}</button>
         </div>
     );
 }
