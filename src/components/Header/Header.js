fe6d53394f49897b09f232ee68b954f9592e3442
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-12 09:24:37 +0100

    Change Header's anchor to button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css';
 function Header(props) {
     return(
         <div className="Header">
-            <a href="" id={props.id1}>{props.title1}</a>
-            <a href="" id={props.id2}>{props.title2}</a>
+            <button href="" id={props.id1}>{props.title1}</button>
+            <button href="" id={props.id2}>{props.title2}</button>
         </div>
     );
 }
